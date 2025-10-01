f00065d67dbb6e82db0f7ebc0a105ae00919564b
miguel <miguelalemania15@gmail.com>
2025-10-01 14:35:12 +0200

    (Wed Oct  1 02:35:08 PM CEST 2025)

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -459,18 +459,11 @@ return require("lazy").setup({
             
             vim.lsp.config("clangd", {})
             
-            vim.lsp.config("rust-analyzer", {
-              cmd = { "/home/miguel/.cargo/bin/rust-analyzer" },
-              filetypes = { "rust" },
-              root_dir = require('lspconfig.util').root_pattern("Cargo.toml", "rust-project.json"),
-            })
-
             vim.lsp.enable({
                   "lua_ls",
                   "clangd",
                   "bashls",
                   "pyright",
-                  "rust-analyzer",
                   "asm_ls",
                   "texlab",
                   "sqlls",
@@ -615,5 +608,36 @@ return require("lazy").setup({
         opts = {},
     },
     -- }}}
-
+    {
+      
+      'mrcjkb/rustaceanvim',
+      version = '^5', -- Use the latest stable version
+      ft = { 'rust' },
+      config = function()
+        vim.g.rustaceanvim = {
+          server = {
+            on_attach = function(_, bufnr)
+              -- Keybinding to toggle inlay hints
+              vim.keymap.set('n', '<Leader>th', function()
+                vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
+              end, { buffer = bufnr })
+            end,
+            settings = {
+              ['rust-analyzer'] = {
+                cargo = { allFeatures = true },
+                checkOnSave = { command = 'clippy' },
+                inlayHints = {
+                  enable = true,               -- Enable inlay hints
+                  typeHints = true,            -- Show type hints
+                  parameterHints = true,       -- Show parameter hints
+                  chainingHints = true,        -- Show hints for chained method calls
+                  closingBraceHints = true,    -- Show closing brace hints
+                  maxLength = 30,              -- Max length of the hints
+                },
+              },
+            },
+          },
+        }
+      end
+    }
 })
